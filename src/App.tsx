9d0cbdf767c26cff764d201446dc6209f3561fb7
bulhiCzar <bulhi.czar@gmail.com>
2024-05-12 14:30:25 +0300

    init react

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {ThirdwebProvider} from "thirdweb/react";
+
+const App = () => {
+
+  return (
+      <ThirdwebProvider>
+    <div className="w-full max-w-xl my-0 mx-auto p-2 text-center">
+        sdf
+    </div>
+      </ThirdwebProvider>
+  )
+}
+
+export default App;
